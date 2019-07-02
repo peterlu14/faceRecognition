f9572062495494c6a3d12a35ef9249fdace097d7
peterlu14 <peterlu810516@gmail.com>
2019-07-03 01:40:33 +0800

    First commit

---
+++ src/Rank/Rank.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+			  <p>
+			  {'Name, your current rank is...'}
+			  </p>
+			</div>
+			<div className='white f2'>
+			  <p>
+			  {'#Num'}
+			  </p>
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
---
+++ src/const/particlesOption.js
@@ -0,0 +1,11 @@
+const particlesOption = {
+    particles: {
+        enable: 'true',
+        type: 'inside',
+        move:{
+            radius: 10
+        }
+    }
+}
+
+export default particlesOption;
